a067d68a7075351807392908bde99a9a0382f904
Adrian Mititelu <mititeluadrian9@gmail.com>
2024-02-01 13:14:16 +0200

    Add some changes

--- src/components/assets/Timeline.jsx
+++ src/components/assets/Timeline.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import timelineElements from "./TimelineElements";
+import timelineElements from "./timelineElements";
 import {
   VerticalTimeline,
   VerticalTimelineElement,
